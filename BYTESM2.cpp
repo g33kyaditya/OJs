a6adc40400c581d4765787e283d2441c0486cf41
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-04-10 22:42:43 +0530

    Easy DP

--- BYTESM2.cpp
+++ BYTESM2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <ostream>
 #include <iterator>
 #include <algorithm>
-#define max(x,y) ((x>y) ? (x) : (y))
+#define MAX(x,y) ((x>y) ? (x) : (y))
+#define LL long long int
 using namespace std;
 int main() {
     
@@ -12,20 +12,41 @@ int main() {
     while (t--) {
         int h,w;
         cin >> h >> w;
-        vector<vector <int> > v(h+1,vector<int>(w+1));
-        for (int i=1; i<=h; i++) {
-            for (int j=1; j<=w; j++) 
-                cin >> v[i][j];
+        int arr[h][w];
+        int dp[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++)
+                cin >> arr[i][j];
         }
-        vector<vector<int> > ans(h+1,vector<int>(w+1));
-        for (int i=1;i<=h;i++) {
-            for (int j=1;j<=w;j++) {
-                ans[i][j] = v[i][j] + max (ans[i-1][j],((j==w-1) ? ans[i-1][j+1] : ans[i-1][j-1]));
+        
+        for (int i = 0; i < w; i++) 
+            dp[0][i] = arr[0][i];
+
+
+        for (int i = 1; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                
+                if (j-1 >= 0 && j+1 < w) {
+                    int k = MAX(dp[i-1][j-1], dp[i-1][j+1]);
+                    dp[i][j] = MAX(dp[i-1][j], k) + arr[i][j];
+                }
+                else if (j-1 >= 0) {
+                    dp[i][j] = MAX(dp[i-1][j], dp[i-1][j-1]) + arr[i][j];
+                }
+                else if (j+1 < w) {
+                    dp[i][j] = MAX(dp[i-1][j], dp[i-1][j+1]) + arr[i][j];
+                }
+                else
+                    dp[i][j] = dp[i-1][j] + arr[i][j];
             }
         }
+        int max = -1000000000;
+        
+        for (int i = 0; i < w; i++) {
+            if (dp[h-1][i] > max)
+                max = dp[h-1][i];
+        }
 
-        cout << ans[h][w] << endl;
-
-
+        cout << max << "\n";
     }
 }
