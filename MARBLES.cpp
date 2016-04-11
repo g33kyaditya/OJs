806019e67eef0319a2eacbebfe2461f4d035a368
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-04-11 22:00:14 +0530

    Ad-hoc, Combinatorics

--- MARBLES.cpp
+++ MARBLES.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
 using namespace std;
-inline long long int fact (long long int num) {
-    long long int f = 1,i=1;
-    if (num == 0)
-        return 1;
-    while (i <= num) {
-        f = f*i;
-        i++;
-    }
-    return f;
+long long int foo(int n, int m){
+    if(m > n-m) m = n - m;
+    
+    long long int ans=1;
+    
+    for(int i = 0; i < m; i++) 
+        ans = ans*(n-i)/(i+1);
+    
+    return ans;
 }
+
 int main() {
+    ios_base::sync_with_stdio(false);
     int t;
+    long long int ans;
     cin >> t;
     while (t--) {
         int n,k;
         cin >> n >> k;
-        long long int ans = 1;
-        if ((n-k) >= (k-1)) {
-            for (int i = n-1;n>n-k;i--)
-                ans = ans*i;
-           long long int val = fact(k-1);
-           ans = ans/val;
-        }
-        else {
-            for (int i = n-1;i>k-1;i--)
-                ans = ans*i;
-            long long int val = fact(n-k);
-            ans = ans/val;
-        }
-
+        ans = foo(n-1, k-1);
         cout << ans << endl;
     }
 }
