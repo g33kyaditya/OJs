15251b3273e6d2c94235315833584e8d1fb68457
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-05-30 16:21:49 +0530

    Basic String Reversal, looping from last character

---
+++ LeetCode/reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseString(string s) {
+        int j = 0;
+        rev = "";
+        for (int i = s.length()-1; i >= 0; i--)
+            rev = rev + s[i];
+        return rev;
+    }
+private:
+    string rev;
+};
