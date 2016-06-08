d0d1fdcc4f20c71c98622a31a06f9158980c9c1e
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-06-09 00:03:40 +0530

    Check if both trees are equal

---
+++ LeetCode/100.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        if (p == NULL && q != NULL)
+            return false;
+            
+        if (q == NULL && p != NULL)
+            return false;
+        
+        if (p == NULL && q == NULL)
+            return true;
+
+        if ((p->val) == (q->val)) {
+            bool leftie = isSameTree(p->left, q->left);
+            bool rightie = isSameTree(p->right, q->right);
+            
+            if (leftie && rightie)
+                return true;
+            else
+                return false;
+        }
+        else 
+            return false;
+    }
+};
