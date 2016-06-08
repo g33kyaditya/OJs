e6062738f6c41cd3dc731148a2a8508f95e0c6d3
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-06-08 21:11:43 +0530

    Move all zeroes to end of array, basic

---
+++ LeetCode/283.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> res;
+    void moveZeroes(vector<int>& nums) {
+           int count = 0;
+            for(int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0)
+                    res.push_back(nums[i]);
+                else 
+                    count++;
+           }
+           
+            while(count--)
+                res.push_back(0);
+            
+            nums = res;
+    }
+};
