fc90b702e1b609e4a05dd0b6cc05508bf42bc449
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-06-09 00:28:55 +0530

    Checking anagrams are equal or not. Used two arrays of sizes 126

---
+++ LeetCode/242.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr1[126] = {0}, arr2[126] = {0};
+        for (int i = 0; i < s.length(); i++)
+            arr1[s[i]]++;
+        
+        for (int j = 0; j < t.length(); j++)
+            arr2[t[j]]++;
+        
+        for (int i = 65; i < 126; i++) {
+            if (arr1[i] != arr2[i])
+                return false;
+        }
+        return true;
+    }
+};
