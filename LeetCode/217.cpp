d12b883da5f74ca74a8d3918a0ce1edbb4996ced
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-06-14 23:12:43 +0530

    Numbers, Map

---
+++ LeetCode/217.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(nums[i]) == mp.end()) {
+                mp[nums[i]] = 1;
+            }
+            else {
+                mp[nums[i]] += 1;
+                if (mp[nums[i]] > 1)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
