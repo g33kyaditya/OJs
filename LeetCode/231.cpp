0bb923e87304a60313bb6f71257ffe29fca58374
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-07-11 01:33:24 +0530

    Power of 2 using bitwise AND, INT_MAX exclusion

---
+++ LeetCode/231.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0 || n == 2147483648 || n == (-2147483648))
+            return false;
+        if ((n)&(n-1))
+            return false;
+        return true;
+    }
+};
