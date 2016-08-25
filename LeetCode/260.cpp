10c138c0d5c516d38a2bb5d49a8a24d27eab5943
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-08-25 23:59:33 +0530

    Hash Table

---
+++ LeetCode/260.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        std::unordered_map<long long int, long long int> m;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        
+        std::unordered_map<long long int, long long int>::iterator it = m.begin();
+        for (it; it != m.end(); it++) {
+            long long int key = it->first;
+            if (m[key] == 1)
+                ans.push_back(key);
+        }
+        return ans;
+    }
+private:
+    vector<int> ans;
+};
