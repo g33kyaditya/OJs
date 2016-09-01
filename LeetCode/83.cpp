b868b6f1a7a03340ba89a88d511f16a6bce56a03
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-02 00:14:36 +0530

    Remove redundant node from sorted Linked List

---
+++ LeetCode/83.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode * temp = head;
+        ListNode * del;
+        if (head == NULL)
+            return head;
+        
+        int val = head->val;
+        ListNode * prev = head;
+        temp = temp->next;
+        while (temp != NULL) {
+            if (val == temp->val) {
+                del = temp;
+                prev->next = temp->next;
+                temp = temp->next;
+                delete del;
+            }
+            else {
+                prev = temp;
+                val = prev->val;
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
+};
