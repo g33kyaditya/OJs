57211c7364dcac4e4209c54360f7a0d13c8426c5
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-04 19:23:13 +0530

    Numbers

---
+++ LeetCode/168.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        
+        string ans = "";
+        char st;
+        while (n > 0) {
+            int rem = n%26;
+            if (rem == 0) {
+                st = 'Z';
+                ans = ans + st;
+                n = n/26 - 1;
+                continue;
+            }
+            
+            st = rem  + 'A' - 1;
+            ans = ans + st;
+            n = n/26;
+        }
+        
+    std::reverse(ans.begin(), ans.end());
+    return ans;
+    }
+};
