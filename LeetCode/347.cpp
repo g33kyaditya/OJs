b4b12b09a2af303ecf5eaf5351adefc49e9d35b0
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-04 21:01:38 +0530

    Multimap

---
+++ LeetCode/347.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> m;
+        m.clear();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        
+        vector<int> ans;
+        
+        
+        multimap<int, int>p;
+        p.clear();
+        
+        map<int,int>::iterator it;
+        for (it = m.begin(); it != m.end(); it++) {
+            int val = it->first;
+            int numOfTimes = it->second;
+            
+            p.insert(std::make_pair(numOfTimes, val));
+        }
+        multimap<int, int>::reverse_iterator pt;
+        int c = 0;
+        for (pt = p.rbegin(); pt != p.rend(); pt++) {
+            if (c >= k)
+                break;
+            
+            ans.push_back(pt->second);
+            c++;
+        }
+        
+        return ans;
+    }
+};
