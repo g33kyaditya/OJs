98a755af9752cc2265c3b15c8fc77187c9b2ea31
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-04 17:23:32 +0530

    Basic hashmap

---
+++ LeetCode/136.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        unordered_map<int, int> m;
+        m.clear();
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        
+        unordered_map<int, int>::iterator it = m.begin();
+        for(it; it != m.end(); it++) {
+            if (it->second != 2)
+                return it->first;
+        }
+        
+        return -1;
+    }
+};
