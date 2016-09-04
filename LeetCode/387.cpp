2e5430dd41b615cff523977a2bc11205f84ab965
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-04 18:15:22 +0530

    HashMap

---
+++ LeetCode/387.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        
+        unordered_map<char, pair<int,int>>m;
+        m.clear();
+        for (int i = 0; i < s.length(); i++) {
+            m[s[i]].first = i;
+            m[s[i]].second++;
+        }
+        int min = 1000000000;
+        unordered_map<char, pair<int, int>>::iterator it = m.begin();
+        for(it; it != m.end(); it++) {
+            pair<int, int> cur = it->second;
+            if (cur.second == 1) {
+                if (cur.first < min)
+                    min = cur.first;
+            }
+        }
+        
+        if (min == 1000000000)
+            return -1;
+        
+        return min;
+    }
+};
