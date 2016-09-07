4b7dcbb208c56f80eb6247af059940c7d9b05dc6
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-07 21:45:27 +0530

    DP, Range Query

---
+++ LeetCode/303.cpp
@@ -0,0 +1,24 @@
+class NumArray {
+public:
+    int *dp;
+    NumArray(vector<int> &nums) {
+        if (nums.empty())
+            return;
+            
+        dp = new int [nums.size()];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+            dp[i] = dp[i-1] + nums[i];
+    }
+
+    int sumRange(int i, int j) {
+       return dp[j] - dp[i-1];
+    }
+
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
