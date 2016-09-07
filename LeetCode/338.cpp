a1b88cb41f03573bb0394936989390eec0529974
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-07 22:24:39 +0530

    DP, Bit Manipulation

--- LeetCode/338.cpp
+++ LeetCode/338.cpp
@@ -1,33 +1,13 @@
-class Solution {
-public:
-    int countOnes(int num) {
-        
-        int power = floor(log2(num));
-        int closest = pow(2, power);
-        int diff = num - closest;
-        int ret = 0;
-        if (closest != 0)
-            ret++;
-        
-        int ctr = 0;
-        while(diff!=0) {
-            if (diff%2 == 1)
-                ctr++;
-            
-            diff = diff/2;
-        }
-        
-        ret = ret + ctr;
-        return ret;
-    }
-
-    vector<int> countBits(int num) {
-        vector<int> ans;
-        for (int i = 0; i <= num; i++) {
-            int r = countOnes(i);
-            ans.push_back(r);
-        }
-        
-        return ans;
-    }
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> dp(num+1, 0);
+    for(int i = 1 ; i <= num ; i++) {
+        if( i%2 == 0 ) 
+            dp[i] = dp[i/2];
+        else
+            dp[i] = dp[i/2]+1;
+    }
+    return dp;
+    }
 };
