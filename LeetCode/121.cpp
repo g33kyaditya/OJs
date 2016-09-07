e03f92f39c8904dcfc9a2e102eb1372562b7c269
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-07 23:29:15 +0530

    DP easy

---
+++ LeetCode/121.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1)
+            return 0;
+        int lowest = prices[0];
+        int best = -1000000000;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < lowest) {
+                lowest = prices[i];
+            }
+            if (prices[i] - lowest > best)
+                best = prices[i] - lowest;
+        }
+        return best;
+    }
+};
