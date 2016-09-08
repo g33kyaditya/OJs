b2b676cbbbea150bbebc4f3b4fd159db2ba8714a
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-08 22:01:39 +0530

    Reverse words in a string

---
+++ LeetCode/151.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+    
+    string A = s;
+    vector<string> v;
+    string str = "";
+    if (s.empty()) {
+        return;
+    }
+    for (int i = 0; i < A.length(); i++) {
+        if (A[i] != ' ')
+            str = str + A[i];
+        else {
+            if (str != "")
+                v.push_back(str);
+            
+            str = "";
+        }
+    }
+    if (str != "" && str != " ")
+        v.push_back(str);
+    
+    A = "";
+    if (v.empty()) {
+        s = "";
+        return;
+    }
+    for (int i = v.size()-1; i > 0; i--) {
+        A = A + v[i];
+        A = A + " ";
+    }
+        A = A + v[0];
+        s = A;
+    }    
+};
