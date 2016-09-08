c1e9cb062cfc635946452f00a89ec4297f8c0dd3
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-09 00:45:08 +0530

    Linked list reorder alternatively

---
+++ LeetCode/143.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (head == NULL)
+            return;
+        ListNode * temp = head;
+        
+        stack<ListNode*> st;
+        
+        ListNode *slow = head, *fast = slow->next;
+        if (fast) {
+            while(slow && fast->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+                if (fast == NULL)
+                    break;
+            }
+        }
+        if (slow->next != NULL)
+            slow = slow->next;
+        
+        while(slow != NULL) {
+            st.push(slow);
+            slow = slow->next;
+        }
+        
+        temp = head;
+        while (!st.empty()) {
+            ListNode * top = st.top();
+            st.pop();
+            if (!st.empty()) {
+                ListNode * nextTop = st.top();
+                nextTop->next = NULL;
+            }
+            ListNode * foo = temp->next;
+            temp->next = top;
+            top->next = foo;
+            
+            if (temp->next != NULL) {
+                if (temp->next->next != NULL)
+                    temp = temp->next->next;
+            }
+        }
+        if (temp != NULL)
+            temp->next = NULL;
+    }
+};
