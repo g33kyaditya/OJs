76a9343e05cca370477d2c4b589adecf7c6477d7
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-08 22:20:18 +0530

    Search in a 2D matrix

---
+++ LeetCode/74.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int c = matrix[0].size() - 1;
+        int R = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][c] >= target) {
+                R = i;
+                break;
+            }
+        }
+        
+        int low = 0;
+        int high = c;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            if (matrix[R][mid] == target)
+                return true;
+            else if (matrix[R][mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    }
+};
