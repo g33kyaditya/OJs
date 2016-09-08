8cfed68d483dead52534be48bbce03e592e452de
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-08 21:25:41 +0530

    Check if parenthesis is valid using stack

---
+++ LeetCode/20.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else {
+                char cur = s[i];
+                if (st.empty())
+                    return false;
+                char top = st.top();
+                
+                if (s[i] == ')' && top != '(')
+                    return false;
+                if (s[i] == '}' && top != '{')
+                    return false;
+                if (s[i] == ']' && top != '[')
+                    return false;
+                st.pop();
+            }
+        }
+        if (!st.empty())
+            return false;
+        return true;
+    }
+};
