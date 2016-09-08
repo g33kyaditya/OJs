726abaf4394ace00c45b2ca1e5dcbe04aac3c73e
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-08 20:57:00 +0530

    Min. depth of a binary tree

---
+++ LeetCode/111.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+    
+        if (root->left == NULL && root->right == NULL)
+            return 1;
+        
+        int left = INT_MAX, right = INT_MAX;
+        
+        if (root->left != NULL)
+            left = minDepth(root->left);
+        
+        if (root->right != NULL)
+            right = minDepth(root->right);
+        
+        return 1 + std::min(left, right);
+    }
+};
