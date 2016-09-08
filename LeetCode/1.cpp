f49fdb97717f34407dbeb87ef14d8302cdc0ca98
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-08 21:13:10 +0530

    Hashtable, check if two nums add to a target

---
+++ LeetCode/1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>m;
+        int first, last;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.count(target-nums[i]) == 1) {
+                first = m[target - nums[i]];
+                last = i;
+                break;
+            }
+            else {
+                m[nums[i]] = i;
+            }
+        }
+        
+        vector<int> ans;
+        ans.push_back(first);
+        ans.push_back(last);
+        return ans;
+    }
+};
