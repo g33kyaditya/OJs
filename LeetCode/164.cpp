4018a266b37d816898892d44657793bf0cf042fc
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-12 00:51:03 +0530

    Maximum gap in an unsorted array in nlogn

---
+++ LeetCode/164.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if (nums.size() < 2)
+            return 0;
+        
+        sort(nums.begin(), nums.end());
+        int max = INT_MIN;
+        for(int i = 1; i < nums.size(); i++) {
+            if (nums[i] - nums[i-1] > max)
+                max = nums[i] - nums[i-1];
+        }
+        return max;
+    }
+};
