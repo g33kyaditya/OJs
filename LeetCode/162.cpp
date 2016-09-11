9fe009501e05f591e37302255860b498f56b1ab9
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-11 17:21:54 +0530

    Find peak elem

---
+++ LeetCode/162.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (i == 0 && (i+1 < nums.size())) {
+                if (nums[i] > nums[i+1])
+                    return i;
+            }
+            
+            else if (i == nums.size()-1 && nums[i] > nums[i-1])
+                return i;
+            
+            else if (nums[i] > nums[i+1] && nums[i] > nums[i-1])
+                return i;
+        }
+        return 0;
+    }
+};
