68c914feac192d292a88fb2acce1261b27d55f35
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-11 16:48:51 +0530

    Get permutations of numbers

---
+++ LeetCode/46.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        
+        bool yes = true;
+        while(yes) {
+            ans.push_back(nums);
+            yes = next_permutation(nums.begin(), nums.end());
+        }
+        
+        return ans;
+    }
+};
