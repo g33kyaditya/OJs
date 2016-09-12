dfe35ce10cbc4f95ea6aee85eba6c06a04c94e26
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-12 11:32:32 +0530

    Swap pairs in linked list

---
+++ LeetCode/24.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+    
+        ListNode * temp = head;
+        ListNode * prev = NULL;
+        while (temp != NULL && temp->next != NULL) {
+            ListNode * curr = temp;
+            ListNode * next = curr->next;
+            curr->next = next->next;
+            next->next = curr;
+            
+            if (prev == NULL)
+                head = next;
+            else
+                prev->next = next;
+            
+            prev = temp;
+            temp = temp->next;
+        }
+        return head;
+    
+    }
+};
