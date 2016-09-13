adb17e1a8a48b4d29b425cf5fe6587905fce6df5
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-13 19:34:34 +0530

    Find pivot in sorted rotated array

---
+++ LeetCode/153.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+    
+        int l = 0;
+        int h = nums.size()-1;
+        int mid;
+        
+        if (nums[l] <= nums[h])
+            return nums[l];
+        
+        while (l <= h) {
+            mid = (l+h)/2;
+            
+            if (nums[mid] > nums[mid+1])
+                return nums[mid+1];
+            
+            else if (nums[l] <= nums[mid])
+                l = mid + 1;
+            
+            else
+                h = mid - 1;
+            
+        }
+        return nums[l];
+    }
+};
