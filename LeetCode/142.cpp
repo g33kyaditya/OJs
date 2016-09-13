502228f3339834f3a80c864b6257795190b48455
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-13 19:57:11 +0530

    Find node where cycle begins in a LL

---
+++ LeetCode/142.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        bool cycle = false;
+        ListNode * slow = head, * fast = head;
+        while (slow && fast && fast->next) {
+        
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if (slow == fast) {
+                cycle = true;
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+    
+            return NULL;
+    }
+};
