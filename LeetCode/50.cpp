79a5cc1495e27cd1285837737ee325f40bf7a278
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-13 13:47:39 +0530

    Implementing power function

---
+++ LeetCode/50.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+    
+        if (n == 0)
+            return 1;
+        
+        if (x == 0)
+            return 0;
+        
+        if (n == 1)
+            return x;
+        
+        if (n == INT_MIN)
+            return (1/x)*(1/x)*myPow((1/x), -(n+2));
+        
+        if (n < 0)
+            return myPow((1/x), -n);
+        
+        if (n%2 == 1)
+            return x*myPow(x, n-1);
+        else
+            return myPow(x*x, n/2);
+        
+    }
+};
