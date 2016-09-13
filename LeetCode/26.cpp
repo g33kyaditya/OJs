70001a72b0a03c2c75e56b861af650963b8bd1df
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-13 22:43:29 +0530

    Removes duplicates from sorted array

---
+++ LeetCode/26.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int i = 0;
+        int n = nums.size();
+        for (int j = 1; j < n; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
