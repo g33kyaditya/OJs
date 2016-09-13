b430312403d557590c077ef16e7da7a8375710f7
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-13 14:17:01 +0530

    Rotating a LL by k positions

---
+++ LeetCode/61.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        if (head == NULL)
+            return head;
+        
+        if (k == 0)
+            return head;
+        
+        int total = 0;
+        ListNode * temp = head;
+        while (temp != NULL) {
+            total++;
+            temp = temp->next;
+        }
+        
+        int f = (k)%total;
+        temp = head;
+        int i = 0;
+        while (i < total-1) {
+            temp = temp->next;
+            i++;
+        }
+        temp->next = head;
+        
+        // Now circular
+        
+        temp = head;
+        int z = total - f;
+        while(z--) {
+            temp = temp->next;
+        }
+        
+        head = temp;
+        temp = head;
+        total--;
+        while(total--) {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        return head;
+    }
+};
