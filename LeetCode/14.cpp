a96342e01b11cefd2c63686ca4206eb262911e10
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-14 21:06:38 +0530

    Longest common prefix of a list of strings

---
+++ LeetCode/14.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        if (strs.empty())
+            return ans;
+            
+        string curr = strs[0];
+        for (int i = 0; i < curr.length(); i++) {
+            char ch = curr[i];
+            bool stat = true;
+            for (int j = 1; j < strs.size(); j++) {
+                string t = strs[j];
+                if (i >= t.length() || t[i] != ch) {
+                    stat = false;
+                    return ans;
+                }
+            }
+            
+            if (stat)
+                ans = ans + ch;
+        }
+        return ans;
+    }
+};
