1debae46b66a4ddf894270368f45c6373973d85e
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-14 22:54:30 +0530

    Palindrome number check, with no extra space

---
+++ LeetCode/9.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        
+        int po = 1;
+        while (x/po >= 10) {
+            po = po*10;
+        }
+        
+        while (x != 0) {
+            int rem = x%10;
+            int first = x/po;
+            if (rem != first)
+                return false;
+
+            x = (x%po)/10;
+            po = po/100;
+        }
+        return true;
+    }
+};
