8a12a5478ad5626e93df25cda30dcaabf29a5908
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-14 20:28:20 +0530

    Generate all subsets, bitwise operators

---
+++ LeetCode/78.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int t = pow(2,n);
+        int i = 0;
+        while (i < t) {
+            vector<int> temp;
+            int cur = i;
+            for (int j = 0; j < n; j++) {
+                if (cur & (1 << j)) {
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+            i++;
+        }
+        return ans;
+    }
+};
