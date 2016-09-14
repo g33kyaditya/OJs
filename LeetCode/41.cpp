b623b7e9732ced9fe252502fbcd06a95b07f7f15
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-15 00:35:51 +0530

    First Missing positive integer in an array

---
+++ LeetCode/41.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int arr[1000000] = {0};
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0)
+                arr[nums[i]]++;
+        }
+        
+        for (int i = 1; i < 1000000; i++) {
+            if (arr[i] == 0)
+                return i;
+        }
+        
+        return 1;
+    }
+};
