9d2976068a225680ddf9a4000d30685e60cd8143
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-15 17:57:22 +0530

    Best Time to Buy and Sell Stocks II

---
+++ LeetCode/122.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
+            return 0;
+        
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.size() - 1) {
+            while (i < prices.size() - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.size() - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+    }
+};
