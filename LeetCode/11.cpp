cacf0f59b8f6e9cd522e32d3e288dfe2e78ce632
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-19 23:20:13 +0530

    Two pointer

---
+++ LeetCode/11.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+    
+        int lo = 0;
+        int hi = height.size()-1;
+        
+        int maxArea = (hi - lo)*(std::min(height[lo], height[hi]));
+        int curArea;
+        while (lo <= hi) {
+            curArea = (hi - lo)*(std::min(height[lo], height[hi]));;
+            if (curArea > maxArea) {
+                maxArea = curArea;
+            }
+            
+            if (height[lo] < height[hi])
+                lo++;
+            else
+                hi--;
+        }
+        
+        return maxArea;
+    }
+};
