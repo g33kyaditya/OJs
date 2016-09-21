4fd0ca554da90a4bc2d06525f08741e1b55b9424
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-22 01:02:10 +0530

    Jump Game - Greedy

---
+++ LeetCode/55.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int jumps = nums[0];
+        int i = 0;
+        while (i < nums.size()-1) {
+            if (jumps == 0)
+                return false;
+            
+            jumps--;
+            i++;
+            jumps = max(jumps, nums[i]);
+        }
+        return true;
+        
+    }
+};
