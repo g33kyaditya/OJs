3b110536041fb7db7a760cc174965e1500d0a613
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-23 00:48:21 +0530

    Gas Station

---
+++ LeetCode/134.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        
+        if (gas.empty() || cost.empty())
+            return -1;
+
+        int index = 0;
+        int sum = 0;
+        int total = 0;
+        
+        for (int i = 0; i < gas.size(); i++) {
+            
+            sum = sum + (gas[i] - cost[i]);
+            total = total + (gas[i] - cost[i]);
+            
+            if (sum < 0) {
+                sum = 0;
+                index = i+1;
+            }
+        }
+        
+        if (total >= 0)
+            return index%(gas.size());
+        else
+            return -1;
+    }
+};
