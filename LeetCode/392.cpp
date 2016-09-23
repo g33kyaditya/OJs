5c78e7628e80457b890ae28338a910061afaebbf
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-23 19:47:09 +0530

    Is Subsequence in O(n)

--- LeetCode/392.cpp
+++ LeetCode/392.cpp
@@ -1,36 +1,27 @@
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-        
-        if (s.empty())
-            return true;
-        
-        int i = 0;
-        int j = 0;
-        bool ans = false;
-        while (i < s.length() && j < t.length()) {
-            if (s[i] != t[j]) {
-                j++;
-                continue;
-            }
-
-            int nextS = i+1;
-            int nextJ = j+1;
-            while (nextS < s.length() && nextJ < t.length()) {
-                if (s[nextS] == t[nextJ]) {
-                    nextS++;
-                    nextJ++;
-                }
-                else
-                    nextJ++;
-            }
-            if (nextS == s.length()) {
-                return true;
-            }
-            i++;
-            j++;
-        }
-        
-        return ans;
-    }
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        if (s.empty())
+            return true;
+        
+        int i = 0;
+        int j = 0;
+        bool ans = false;
+        while (i < s.length() && j < t.length()) {
+            
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+                continue;
+            }
+            else
+                j++;
+        }
+        
+        if (i == s.length())
+            ans = true;
+        
+        return ans;
+    }
 };
