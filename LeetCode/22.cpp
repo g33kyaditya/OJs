2f768fc8fe053d0a0e92842f6db1ebcb8b8d8820
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-23 21:00:09 +0530

    Generate Parantheses

---
+++ LeetCode/22.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    void gen(int left, int right, string str, vector<string>& ans) {
+        if (right == 0) {
+            ans.push_back(str);
+            return;
+        }
+        
+        if (left > 0) {
+            gen(left-1, right, str + "(", ans);
+            if (left < right)
+                gen(left, right-1, str + ")", ans);
+        }
+        else
+            gen(left, right-1, str + ")", ans);
+    }
+
+    vector<string> generateParenthesis(int n) {
+        
+        vector<string> ans;
+        string ret = "";
+        gen(n, n, ret, ans);
+        return ans;
+    }
+};
