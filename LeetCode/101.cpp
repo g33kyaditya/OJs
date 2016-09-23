1867635657e8d5902b69ed095846af739831a9da
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-23 22:46:46 +0530

    Check if tree is symmetric

---
+++ LeetCode/101.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool isMirror(TreeNode * a, TreeNode * b) {
+        
+        if (a == NULL && b == NULL)
+            return true;
+        
+        if (a && b && a->val == b->val) {
+            return isMirror(a->left, b->right) && isMirror(a->right, b->left);
+        }
+        return false;
+        
+    }
+
+    bool isSymmetric(TreeNode* root) {
+    
+        if (root == NULL)
+            return true;
+        
+        if (isMirror(root, root))
+            return true;
+        return false;
+    }
+};
