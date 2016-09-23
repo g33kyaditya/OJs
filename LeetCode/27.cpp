bd7af28e3dc2d7c579516efb9b78568a0033a08f
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-23 22:26:28 +0530

    Removed element

---
+++ LeetCode/27.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int start = 0;
+        int last = nums.size()-1;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val)
+                nums[start++] = nums[i];
+        }
+        return start;
+           
+    }
+};
