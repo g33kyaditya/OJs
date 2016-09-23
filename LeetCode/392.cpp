bf447b7e794f4b4bd3914ed5af0eb219c9a9be2a
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-23 19:44:02 +0530

    Is Subsequence

---
+++ LeetCode/392.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        if (s.empty())
+            return true;
+        
+        int i = 0;
+        int j = 0;
+        bool ans = false;
+        while (i < s.length() && j < t.length()) {
+            if (s[i] != t[j]) {
+                j++;
+                continue;
+            }
+
+            int nextS = i+1;
+            int nextJ = j+1;
+            while (nextS < s.length() && nextJ < t.length()) {
+                if (s[nextS] == t[nextJ]) {
+                    nextS++;
+                    nextJ++;
+                }
+                else
+                    nextJ++;
+            }
+            if (nextS == s.length()) {
+                return true;
+            }
+            i++;
+            j++;
+        }
+        
+        return ans;
+    }
+};
