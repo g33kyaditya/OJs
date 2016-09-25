50e1735c6bd0dca4ed5053e73e5d8eb3d65c900c
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-25 16:45:01 +0530

    Binary Tree Inorder Traversal using stack

---
+++ LeetCode/94.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        
+        stack<TreeNode*> s;
+        while (!s.empty() || root != NULL) {
+            
+            if (root != NULL) {
+                s.push(root);
+                root = root->left;
+            }
+            else {
+                root = s.top();
+                s.pop();
+                ans.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return ans;
+           
+    }
+};
