2a9ba9fb4a6104df48373721dd3d32467f2858f3
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-25 18:12:26 +0530

    Finds sqrt of a number

---
+++ LeetCode/69.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        long long int low = 1;
+        long long int high = x;
+        long long int mid = 0;
+        long long int last = 0;
+        if (x < 0)
+            return INT_MIN;
+
+        while (low <= high) {
+            mid = (long long)(low + high)/2;
+            if (mid*mid == x) {
+                return mid;
+            }
+            else if (mid*mid < x) {
+                last = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return last;
+    }
+};
