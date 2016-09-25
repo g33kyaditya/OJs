8aee56afbca0f290083330478906f111a047b229
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-25 18:11:55 +0530

    Check if number is perfect square

---
+++ LeetCode/367.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+        if (num == 0)
+            return false;
+        
+        long long int low = 1;
+        long long int high = num;
+        long long int mid = 0;
+        long long int last = mid;
+        
+        while (low <= high) {
+            mid = (low + high)/2;
+            if (mid*mid == num)
+                return true;
+            else if (mid*mid < num) {
+                last = mid;
+                low = mid+1;
+            }
+            else
+                high = mid - 1;
+        }
+        
+        if (last*last != num)
+            return false;
+        
+        return true;
+    }
+};
