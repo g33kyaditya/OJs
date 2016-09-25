1c060dfe687f8defe8b438bad43204a3d2afb441
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-25 16:53:14 +0530

    Missing number in an array

---
+++ LeetCode/268.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = (n*(n+1))/2;
+        int cur = 0;
+        for (int i = 0; i < n; i++)
+            cur = cur + nums[i];
+        
+        return expectedSum - cur;
+    }
+};
