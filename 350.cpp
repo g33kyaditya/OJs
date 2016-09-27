1d4367051dc04517e1c0bc96068f28070dcd3eda
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-27 23:50:04 +0530

    Intersection of Two Arrays II

---
+++ 350.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    
+        vector<int> ans;
+        if (nums1.empty() || nums2.empty())
+            return ans;
+        
+        unordered_map<int, int> m1;
+        for (int i = 0; i < nums1.size(); i++)
+            m1[nums1[i]]++;
+        
+        for (int i = 0; i < nums2.size(); i++) {
+            if (m1[nums2[i]] > 0) {
+                m1[nums2[i]]--;
+                ans.push_back(nums2[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
