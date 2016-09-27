24a8b9d523148d188a8f75d1d0afe70a80bf3a49
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-28 00:35:49 +0530

    Happy Number

---
+++ 202.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    int sum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum = sum + (n%10)*(n%10);
+            n = n/10;
+        }
+        return sum;
+    }
+    
+    
+    bool isHappy(int n) {
+        
+        if (n == 1)
+            return true;
+        
+        set<int> s;
+        while(s.find(n) == s.end()) {
+            s.insert(n);
+            n = sum(n);
+            
+            if (n == 1)
+                return true;
+        }
+        return false;
+    }
+};
