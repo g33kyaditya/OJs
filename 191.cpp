e821760ea9143fd95db2468af12cc703ff3dc06c
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-28 00:16:57 +0530

    Number of 1 bits

---
+++ 191.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int a = 32;
+        int count = 0;
+        while(a--) {
+            if (n&(01))
+                count++;
+            
+            n = n >> 1;
+        }
+        return count;
+    }
+};
