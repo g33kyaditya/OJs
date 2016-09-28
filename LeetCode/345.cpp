407066235311a6c6a16ffc5989c12306033f4580
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-28 20:32:40 +0530

    Reverse vowels in a LL

---
+++ LeetCode/345.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<char> vowels;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                vowels.push_back(s[i]);
+        }
+        
+        reverse(vowels.begin(), vowels.end());
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                s[i] = vowels[index++];
+        }
+        
+        return s;
+    }
+};
