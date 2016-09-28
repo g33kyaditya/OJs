fe381caf32a50aee0d4496f51d360c100fdff870
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-28 23:14:38 +0530

    Number of trailing zeroes in fact

---
+++ LeetCode/172.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        long long int i = 5;
+        long long int count = 0;
+        while (i <= n) {
+            count = count + n/i;
+            i = i*5;
+        }
+        return count;
+    }
+};
