a348664a4293041350036f5d79030dafbe719e5e
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-28 22:05:15 +0530

    Check number is power of 4 using bitwise

--- LeetCode/342.cpp
+++ LeetCode/342.cpp
@@ -1,20 +1,32 @@
-class Solution {
-public:
-    bool isPowerOfFour(int num) {
-        
-        if (num == 0)
-            return false;
-        
-        if (num == 1)
-            return true;
-        
-        while (num%4 == 0) {
-            num = num/4;
-        }
-        if (num == 1)
-            return true;
-        else
-            return false;
-           
-    }
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        
+        if (num == 0)
+            return false;
+        
+        if (num == 1)
+            return true;
+        
+        bool alreadySet = false;
+        int count = 0;
+        while (num != 0) {
+            if (alreadySet && num&1)
+                return false;
+            
+            if (num&(1)) {
+                alreadySet = true;
+            }
+            else
+                count++;
+            
+            num = num >> 1;
+        }
+        
+        if (count%2 == 0 && alreadySet)
+            return true;
+        else
+            return false;
+           
+    }
 };
