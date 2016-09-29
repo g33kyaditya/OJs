2e3828fea330489a46dfc06567287545b354da55
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-29 22:40:09 +0530

    Isomorphic Strings

---
+++ LeetCode/205.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> m;
+        bool chars[126];
+        memset(chars, false, sizeof(chars));
+        for (int i = 0; i < s.length(); i++) {
+            if (m.find(s[i]) != m.end()) {
+                if (m[s[i]] != t[i])
+                    return false;
+            }
+            else {
+                if (chars[t[i]]) {
+                    auto it = m.begin();
+                    for (it; it != m.end(); it++) {
+                        if (it->second == t[i] && it->first != s[i])
+                            return false;
+                    }
+                }
+                m[s[i]] = t[i];
+                chars[t[i]] = true;
+            }
+        }
+        return true;
+    }
+};
