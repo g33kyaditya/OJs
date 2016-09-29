64ba376e7d8eb090d81fb68a98b34f47e0921b92
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-29 20:46:21 +0530

    Binary Search to Guess Number

---
+++ LeetCode/374.cpp
@@ -0,0 +1,24 @@
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long int low = 1;
+        long long int high = n;
+        long long int mid;
+        while (low <= high) {
+            mid = (low + high)/2;
+            
+            if (guess(mid) == 0)
+                return mid;
+            else if (guess(mid) == -1)
+                high = mid - 1;
+            else if (guess(mid) == 1)
+                low = mid + 1;
+        }
+        return mid;
+    }
+};
