4cdf70db712920bcb6f2edf8bf6109ff9f666997
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-29 22:10:56 +0530

    Path sum to a leaf node in binary tree

---
+++ LeetCode/112.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == NULL)
+            return false;
+        
+        if (root->left == NULL && root->right == NULL) {
+            if (sum - root->val == 0)
+                return true;
+        }
+        
+        bool ret = false;
+        if (root->left) {
+            ret = hasPathSum(root->left, sum - root->val);
+            if (ret)
+                return true;
+        }
+        if (root->right) {
+            ret = hasPathSum(root->right, sum - root->val);
+            if (ret)
+                return true;
+        }
+        
+        return false;
+    }
+};
