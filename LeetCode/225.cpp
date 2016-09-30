02917cd5a922bdb4ac209e2be574d8f61bacf866
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-30 19:37:02 +0530

    Implement Stack using two queues

---
+++ LeetCode/225.cpp
@@ -0,0 +1,46 @@
+class Stack {
+public:
+    // Push element x onto stack.
+    void push(int x) {
+        q.push(x);
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+        while (q.size() != 1) {
+            temp.push(q.front());
+            q.pop();
+        }
+        q.pop();
+        while (!temp.empty()) {
+            q.push(temp.front());
+            temp.pop();
+        }
+    }
+
+    // Get the top element.
+    int top() {
+        while (q.size() != 1) {
+            temp.push(q.front());
+            q.pop();
+        }
+        int ret = q.front();
+        temp.push(ret);
+        q.pop();
+        while (!temp.empty()) {
+            q.push(temp.front());
+            temp.pop();
+        }
+        return ret;
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        if (q.empty())
+            return true;
+        return false;
+    }
+private:
+    queue<int> q;
+    queue<int> temp;
+};
