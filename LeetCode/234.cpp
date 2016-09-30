3d546052d9e38a02913be628a04ecae947d97e18
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-30 22:29:44 +0530

    Check if LL is palindrome using stack

---
+++ LeetCode/234.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        ListNode * temp = head;
+        while (temp != NULL) {
+            st.push(temp->val);
+            temp = temp->next;
+        }
+        
+        while (!st.empty() && head != NULL) {
+            if (head->val != st.top())
+                return false;
+            
+            head = head->next;
+            st.pop();
+        }
+        return true;
+    }
+};
