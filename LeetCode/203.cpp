d05b0a0f525270e8e6a9c78656d0cedaa643b98a
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-30 22:51:51 +0530

    Remove LL element having target value

---
+++ LeetCode/203.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode * temp = head;
+        ListNode * back = head;
+        
+        while (head && head->val == val)
+            head = head->next;
+        
+        while (temp != NULL) {
+            if (temp->val == val) {
+                back->next = temp->next;
+                temp = temp->next;
+                continue;
+            }
+            back = temp;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
