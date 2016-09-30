05b32fb44e45f74a577de7f7b028e837cf5b6305
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-09-30 22:40:16 +0530

    Length of last word

---
+++ LeetCode/58.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int count = 0;
+        string str;
+        int last = s.length();
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (s[i] == ' ')
+                last--;
+            else
+                break;
+        }
+        str = s.substr(0, last);
+        for (int i = str.length()-1; i >=0; i--) {
+            if (s[i] == ' ') {
+                count = str.length()-1 - i;
+                return count;
+            }
+            else if (i == 0) {
+                count = str.length();
+                return count;
+            }
+        }
+        return count;
+    }
+};
