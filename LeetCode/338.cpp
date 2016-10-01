c20cd8627cc68eedab42524aab4369113d634b00
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-01 23:35:36 +0530

    Counting Bits

--- LeetCode/338.cpp
+++ LeetCode/338.cpp
@@ -2,12 +2,18 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> dp(num+1, 0);
-    for(int i = 1 ; i <= num ; i++) {
-        if( i%2 == 0 ) 
-            dp[i] = dp[i/2];
-        else
-            dp[i] = dp[i/2]+1;
-    }
-    return dp;
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            if (i&(i-1) == 0) {
+                dp[i] = 1;
+            }
+            else {
+                int closest = floor(log2(i));
+                closest = pow(2, closest);
+                dp[i] = 1 + dp[i-closest];
+            }
+        }
+        return dp;
     }
 };
