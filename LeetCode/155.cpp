e84f238713ed8528a60af65ea29e35be218e7fb3
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-01 18:13:14 +0530

    Min Stack that also gives the minimum element

---
+++ LeetCode/155.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+
+    }
+    
+    void push(int x) {
+        st.push(x);
+        if (m.empty() || x <= m.top())
+            m.push(x);
+    }
+    
+    void pop() {
+        if (st.top() == m.top()) {
+            m.pop();
+            st.pop();
+        }
+        else
+            st.pop();
+    }
+    
+    int top() {
+        return st.top();    
+    }
+    
+    int getMin() {
+        return m.top();
+    }
+private:
+    stack<long long int> st;
+    stack<long long int> m;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
