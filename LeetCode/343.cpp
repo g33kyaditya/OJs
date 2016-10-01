162be7048aeb9799d72d3535e3c51184d75b1507
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-02 00:44:32 +0530

    Integer Break

---
+++ LeetCode/343.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        switch (n) {
+            case 0: return 0;
+                    break;
+            
+            case 1: return 0;
+            
+            case 2: return 1;
+            
+            case 3: return 2;
+            
+            case 4: return 4;
+            
+            case 5: return 6;
+            
+            case 6: return 9;
+        }
+        
+        int dp[n+1]; 
+        dp[0] = 0;
+        dp[1] = 0; 
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 4;
+        dp[5] = 6;
+        dp[6] = 9;
+        for (int i = 7; i <= n; i++)
+            dp[i] = dp[i-3]*3;
+        
+        return dp[n];
+    }
+};
