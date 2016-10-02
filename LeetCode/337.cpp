b61fbd789cb60d15765ac5bb31ff5b550269a568
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-02 16:25:52 +0530

    House Robber III

---
+++ LeetCode/337.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        
+        if (root->left == NULL && root->right == NULL)
+            return root->val;
+        
+        int left = 0, right = 0;
+        
+        if (root->left)
+            left = rob(root->left->left) + rob(root->left->right);
+        if (root->right)
+            right = rob(root->right->left) + rob(root->right->right);
+        
+        int max = 0;
+        max = left + right + root->val;
+        
+        left = rob(root->left);
+        right = rob(root->right);
+        
+        max = std::max(max, left + right);
+        return max;
+    }
+};
