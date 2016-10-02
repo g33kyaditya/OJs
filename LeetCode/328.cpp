41fb276bdfa1195f7dc6995481754ac3e4632f63
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-02 14:59:44 +0530

    Group LL by even and odd

---
+++ LeetCode/328.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == NULL || head->next == NULL || head->next->next == NULL)
+            return head;
+        
+        ListNode * oddHead = head;
+        ListNode * evenHead = head->next;
+        
+        ListNode * cur = oddHead;
+        ListNode * next = evenHead;
+        while (cur != NULL && next != NULL) {
+            ListNode * temp = next;
+            cur->next = next->next;
+            cur = temp;
+            next = cur->next;
+        }
+        
+        ListNode * temp = oddHead;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = evenHead;
+        
+        return oddHead;
+    }
+};
