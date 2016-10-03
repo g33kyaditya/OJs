cc0724b00623b96f1c4c55b86353fc923c0b0f7f
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-03 23:58:43 +0530

    Combinations

---
+++ LeetCode/77.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void foo(int start, int k, vector<int> ret, vector<vector<int>>&ans, vector<int>& pool) {
+        if (ret.size() == k) {
+            ans.push_back(ret);
+            return;
+        }
+        
+        for (int i = start; i < pool.size(); i++) {
+            ret.push_back(i);
+            foo(i+1, k, ret, ans, pool);
+            ret.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> ret;
+        vector<int> pool(n+1);
+        for (int i = 1; i <= n; i++)
+            pool[i] = i;
+
+        foo(1, k, ret, ans, pool);
+        return ans;
+    }
+};
