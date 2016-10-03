fec40716aff861608312580d6539bfd1ab8fb33b
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-03 21:22:42 +0530

    Improved using Binary Search

--- LeetCode/153.cpp
+++ LeetCode/153.cpp
@@ -1,27 +1,14 @@
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-    
-        int l = 0;
-        int h = nums.size()-1;
-        int mid;
-        
-        if (nums[l] <= nums[h])
-            return nums[l];
-        
-        while (l <= h) {
-            mid = (l+h)/2;
-            
-            if (nums[mid] > nums[mid+1])
-                return nums[mid+1];
-            
-            else if (nums[l] <= nums[mid])
-                l = mid + 1;
-            
-            else
-                h = mid - 1;
-            
-        }
-        return nums[l];
-    }
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+        ans.push_back(0);
+        if (n == 0)
+            return ans;
+
+        for (int i = 1; i < pow(2,n); i++) {
+            ans.push_back(i ^ (i/2));
+        }
+        return ans;
+    }
 };
