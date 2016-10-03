e58fba55fc7f63369399e878b978dc8f4850e7a3
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-03 20:47:19 +0530

    Gray Code

---
+++ LeetCode/89.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans;
+        ans.push_back(0);
+        if (n == 0)
+            return ans;
+
+        for (int i = 1; i < pow(2,n); i++) {
+            ans.push_back(i ^ (i/2));
+        }
+        return ans;
+    }
+};
