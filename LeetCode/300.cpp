b3cf63b5401d7b07f9e76cce3dccd837ca334da0
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-04 16:35:57 +0530

    Longest Increasing Subsequence

---
+++ LeetCode/300.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        int n = nums.size();
+        vector<int>lis(n, 0);
+        int ans = INT_MIN;
+        for (int j = 0; j < n; j++) {
+            lis[j] = 1;
+            for (int i = 0; i < j; i++) {
+                if (nums[j] > nums[i]) {
+                    lis[j] = std::max(lis[j], lis[i]+1);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (lis[i] > ans)
+                ans = lis[i];
+        }
+        return ans;
+    }
+};
