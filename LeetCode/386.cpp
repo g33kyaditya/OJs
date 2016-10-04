c56300c814ff426446a3332aada8cd5fecdfd0ce
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-04 21:40:30 +0530

    Lexographical Numbers DFS

---
+++ LeetCode/386.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void dfs(int cur, int n, vector<int>& res) {
+        for (int i = 0; i <= 9; i++) {
+            int temp = cur*10 + i;
+            if (temp > n)
+                break;
+            
+            res.push_back(temp);
+            dfs(temp, n, res);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for (int i = 1; i<=n && i<=9; i++) {
+            ans.push_back(i);
+            dfs(i, n, ans);
+        }
+        return ans;
+    }
+};
