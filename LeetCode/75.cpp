6c05d513f72518380f66eb3d13dc182e851365a4
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-04 19:17:20 +0530

    Improved with Dutch National Flag Problem

--- LeetCode/75.cpp
+++ LeetCode/75.cpp
@@ -1,6 +1,26 @@
-class Solution {
-public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-    }
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0, mid = 0, hi = nums.size()-1;
+        while (mid <= hi) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[lo];
+                nums[lo] = temp;
+                lo++;
+                mid++;
+            }
+            
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[hi];
+                nums[hi] = temp;
+                hi--;
+            }
+        }
+    }
 };
