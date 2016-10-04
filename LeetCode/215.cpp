176eefc0da8c3251528b293ea75b1735d6af7537
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-04 20:31:56 +0530

    Kth largest element in an array

---
+++ LeetCode/215.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        std::priority_queue<int, std::vector<int>, std::greater<int> > heap;
+        for (int i = 0; i < nums.size(); i++)
+            heap.push(nums[i]);
+        
+        int ans;
+        int c = nums.size()-k+1;
+        while ( c--) {
+            ans = heap.top();
+            heap.pop();
+        }
+        return ans;
+    }
+};
