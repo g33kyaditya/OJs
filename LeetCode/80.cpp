b5a7a9d377cf3e44f547af2f0971f4aef7225dae
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-05 22:20:08 +0530

    Two pointer Remove Duplicates from sorted array ii

--- LeetCode/80.cpp
+++ LeetCode/80.cpp
@@ -1,35 +1,23 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        
-        if (nums.empty())
-            return 0;
-        
-        int n = nums.size();
-        vector<int> ans;
-        if (n == 2 || n == 1)
-            return nums.size();
-        
-        int ct = 0;
-        if (n > 0) {
-            ans.push_back(nums[0]);
-            ct++;
-        }
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i-1]) {
-                ct++;
-                if (ct <= 2)
-                    ans.push_back(nums[i]);
-            }
-            else {
-                ans.push_back(nums[i]);
-                ct = 1;
-            }
-        }
-        
-        nums.clear();
-        nums = ans;
-        return nums.size();
-    }
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        
+        int index = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                count++;
+                if (count >= 3) {
+                    continue;
+                }
+            }
+            else {
+                count = 1;
+            }
+            nums[index++] = nums[i];
+        }
+        return index;
+    }
 };
