d330292b24dc008c0d114b55b0886a9e0c0f08cb
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-05 21:16:24 +0530

    Coin change problem

---
+++ LeetCode/279.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> vec;
+        for (int i = 1; i*i <= n; i++)
+            vec.push_back(i*i);
+        
+        vector<int>dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < vec.size(); j++) {
+                if (vec[j] <= i) {
+                    dp[i] = std::min(dp[i], 1 + dp[i - vec[j]]);
+                }
+            }
+        }
+        return dp[n];
+        
+    }
+};
