75ae4eb8eff4610ed659a7c23b25840205a8ad3a
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-06 00:12:30 +0530

    H-Index

---
+++ LeetCode/274.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        if (citations.empty())
+            return 0;
+        
+        std::sort(citations.begin(), citations.end());
+        std::reverse(citations.begin(), citations.end());
+        int i;
+        for (i = 0; i < citations.size(); i++) {
+            if (i + 1 > citations[i])
+                return i;
+        }
+         
+        return i;
+    }
+};
