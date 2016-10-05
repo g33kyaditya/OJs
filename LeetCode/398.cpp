d06fbb9f108edf7f41f3a9222e9e4225e6511b04
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-05 19:23:57 +0530

    Pick a random index from array

---
+++ LeetCode/398.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    Solution(vector<int> nums) {
+       arr = nums;
+    }
+    
+    int pick(int target) {
+        long long int count = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == target)
+                count++;
+        }
+        
+        int index = rand()%count + 1;
+        count = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (target == arr[i])
+                count++;
+            
+            if (count == index)
+                return i;
+        }
+        return 0;
+    }
+private:
+    vector<int>arr;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
