cccecba8b9a6f705079624c5c4513e40d78e4c73
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-07 19:36:41 +0530

    Bitwise AND in a given range

---
+++ LeetCode/201.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int bits = 0;
+        while (1) {
+            if (n > m)
+                bits++;
+            else {
+                return m << bits;
+            }
+            
+            m >>= 1;
+            n >>= 1;
+        }
+        return m;
+    }
+};
