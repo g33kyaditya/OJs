4ea3d91fe40f41c11e047da0cfa38718592516f7
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-07 17:55:50 +0530

    Subsets II

---
+++ LeetCode/90.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>>ans;
+        vector<vector<int>>ret;
+        
+        if (nums.empty())
+            return ret;
+        int total = pow(2, nums.size());
+        int t = nums.size();
+        for (int i = 0; i < total; i++) {
+            vector<int>cur;
+            int temp = i, ctr = 0;
+            while(ctr < t) {
+                if (temp&1)
+                    cur.push_back(nums[ctr]);
+                
+                temp = temp >> 1;
+                ctr++;
+            }
+            sort(cur.begin(), cur.end());
+            ans.insert(cur);
+        }
+        
+        auto it = ans.begin();
+        for(; it != ans.end(); it++) {
+            ret.push_back(*it);
+        }
+        return ret;
+    }
+};
