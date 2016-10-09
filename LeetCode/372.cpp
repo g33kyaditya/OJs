7535e895aa455892c796846f332dcd3ef5e4903d
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-09 14:09:36 +0530

    Super Pow

---
+++ LeetCode/372.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int myPow( int x, int y ){
+        int ans = 1;
+        while(y-- )
+            ans = ans * x % 1337;
+        return ans;
+    }
+
+
+    int superPow(int a, vector<int>& b) {
+        a = a % 1337;
+        long long int ans = 1;
+        while( b.size() ){
+            ans = ans * myPow(a, b.back()) % 1337;
+            b.pop_back();
+            a = myPow( a, 10 );
+        }
+        return ans;
+    }
+};
