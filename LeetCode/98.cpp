c9fb26827aa20b2e2463f0346b388e6b54ecb054
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-11 00:48:23 +0530

    Validate BST

---
+++ LeetCode/98.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode * root, long long int lo, long long int hi) {
+        if (root == NULL)
+            return true;
+        
+        if (root->val >= hi || root->val <= lo)
+            return false;
+        
+        if (!check(root->left, lo, root->val) || !check(root->right, root->val, hi))
+            return false;
+        
+        return true;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        if (root == NULL)
+            return true;
+        
+        return check(root, -1000000000000, 10000000000000);
+    }
+};
