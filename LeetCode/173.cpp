a6eb8ac7aa635f3b15fe3a4061ce7eb53dd9b9f7
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-11 23:42:25 +0530

    BST Iterator

---
+++ LeetCode/173.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+        pushS(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        if (st.empty())
+            return false;
+        return true;
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode * cur = st.top();
+        st.pop();
+        pushS(cur->right);
+        return cur->val;
+    }
+    
+    void pushS(TreeNode * root) {
+        while (root != NULL) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+private:
+    stack<TreeNode*> st;
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
