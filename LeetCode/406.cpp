c53be5ea7d298af3b69ae7ba7f38a94ff72a204a
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-11 13:47:34 +0530

    Queue Reconstruction by Height

---
+++ LeetCode/406.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    static bool comp(pair<int, int> a, pair<int, int>b) {
+        if (a.first > b.first)
+            return true;
+        else if (a.first < b.first)
+            return false;
+        else {
+            if (a.second < b.second)
+                return true;
+            return false;
+        }
+    }
+
+    vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
+        if (people.empty())
+            return people;
+        
+        vector<pair<int,int>>res;
+        sort(people.begin(), people.end(), comp);
+        
+        stack<pair<int,int>> st;
+        for (int i = 0; i < people.size(); i++) {
+            if (people[i].second > res.size())
+                res.push_back(people[i]);
+            else {
+                while (people[i].second < res.size()) {
+                    st.push(res.back());
+                    res.pop_back();
+                }
+                res.push_back(people[i]);
+                while (!st.empty()) {
+                    res.push_back(st.top());
+                    st.pop();
+                }
+            }
+        }
+        
+        return res;
+    }
+};
