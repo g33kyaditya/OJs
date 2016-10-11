b93f7e9ae3c0816095a93b9ddddbba804a62c535
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-11 14:02:42 +0530

    Count numbers with unique digits

---
+++ LeetCode/357.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        
+        if (n == 0)
+            return 1;
+        
+        if (n == 1)
+            return 10;
+        
+        int dp[n+1];
+        dp[0] = 1;
+        
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = 9;
+            for (int j = 9; j >= 11 - i; j--)
+                dp[i] = dp[i]*j;
+        }
+        
+        for (int i = 0; i < n+1; i++)
+            sum += dp[i];
+
+        return sum;
+    }
+};
