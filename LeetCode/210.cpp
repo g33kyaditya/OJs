d8eb1681d28546537a27cb9c8520bb06d0c8cee4
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-10-14 23:59:46 +0530

    Course Schedule II using DFS

--- LeetCode/210.cpp
+++ LeetCode/210.cpp
@@ -1,39 +1,156 @@
 class Solution {
+
 public:
-    vector<int> findOrder(int n, vector<pair<int, int>>& pre) {
-        vector<int> ans;
-        if (n == 0)
-            return ans;
-        
-        int incoming[n] = {0};
-        
-        for (int i = 0; i < pre.size(); i++) {
-            pair<int, int> cur = pre[i];
-            incoming[cur.first]++;
-        }
-        bool visited[n] = {false};
-        for (int i = 0; i < n; i++) {
-            if (incoming[i] == 0 && !visited[i]) {
-                ans.push_back(i);
-                visited[i] = true;
-                int next = i;
-                for (int j = 0; j < pre.size(); j++) {
-                    pair<int, int> cur = pre[j];
-                    if (cur.second == i) {
-                        incoming[cur.first]--;
-                        if (incoming[cur.first] == 0) {
-                            next = min(next, cur.first);
-                        }
-                    }
-                }
-                if (next != i)
-                    i = next - 1;
+
+    void DFS(int v, bool * visited, vector<int>& ans, bool * onStack) {
+
+        visited[v] = true;
+
+        onStack[v] = true;
+
+        auto it = adj[v].begin();
+
+        for (it; it != adj[v].end(); it++) {
+
+            if (!visited[*it]) {
+
+                DFS(*it, visited, ans, onStack);
+
             }
+
+            else if (onStack[*it]) {
+
+                ans.clear();
+
+                return;
+
+            }
+
         }
+
+        onStack[v] = false;
+
+        ans.push_back(v);
+
+    }
+
+    vector<int> findOrder(int n, vector<pair<int, int>>& pre) {
+
+        vector<int> ans;
+
+        if (n == 0)
+
+            return ans;
+
         
-        if (ans.size() != n)
+
+        buildGraph(n, pre);
+
+        bool visited[n] = {false};
+
+        bool onStack[n] = {false};
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i])
+
+                DFS(i, visited, ans, onStack);
+
+        }
+
+        if (ans.size() != n) {
+
             ans.clear();
 
+            return ans;
+
+        }
+
+
+
+        reverse(ans.begin(), ans.end());
+
         return ans;
+
     }
+
+    
+
+    void buildGraph(int n, vector<pair<int, int>>& edges) {
+
+        adj = new list<int>[n];
+
+        
+
+        for (int i = 0; i < edges.size(); i++) {
+
+            pair<int,int> cur = edges[i];
+
+            adj[cur.second].push_back(cur.first);
+
+        }
+
+    }
+
+private:
+
+    list<int> *adj;
+
+};
+
+
+===========================================================================================================================================
+
+//Using DFS
+
+
+class Solution {
+public:
+    void DFS(int v, bool * visited, vector<int>& ans, bool * onStack) {
+        visited[v] = true;
+        onStack[v] = true;
+        auto it = adj[v].begin();
+        for (it; it != adj[v].end(); it++) {
+            if (!visited[*it]) {
+                DFS(*it, visited, ans, onStack);
+            }
+            else if (onStack[*it]) {
+                ans.clear();
+                return;
+            }
+        }
+        onStack[v] = false;
+        ans.push_back(v);
+    }
+    vector<int> findOrder(int n, vector<pair<int, int>>& pre) {
+        vector<int> ans;
+        if (n == 0)
+            return ans;
+        
+        buildGraph(n, pre);
+        bool visited[n] = {false};
+        bool onStack[n] = {false};
+        for (int i = 0; i < n; i++) {
+            if (!visited[i])
+                DFS(i, visited, ans, onStack);
+        }
+        if (ans.size() != n) {
+            ans.clear();
+            return ans;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    
+    void buildGraph(int n, vector<pair<int, int>>& edges) {
+        adj = new list<int>[n];
+        
+        for (int i = 0; i < edges.size(); i++) {
+            pair<int,int> cur = edges[i];
+            adj[cur.second].push_back(cur.first);
+        }
+    }
+private:
+    list<int> *adj;
 };
