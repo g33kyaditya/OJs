bc827ff571a3ae7460ff70cad107355279cc2592
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-11-08 17:15:21 +0530

    Used better approach

--- LeetCode/81.cpp
+++ LeetCode/81.cpp
@@ -8,3 +8,39 @@ public:
         return false;
     }
 };
+
+
+
+============================================================
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+
+        int lo = 0;
+        int hi = nums.size()-1;
+        int mid;
+        while (lo <= hi) {
+            mid = (lo + hi)/2;
+            if (nums[mid] == target)
+                return true;
+
+            else if (nums[lo] < nums[mid]) {
+                if (target >= nums[lo] && target < nums[mid])
+                    hi = mid - 1;
+                else
+                    lo = mid + 1;
+            } else if (nums[lo] > nums[mid]) {
+                if(target > nums[mid] && target <= nums[hi])
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            } else {
+                lo++;
+            }
+        }
+        
+        return false;
+    }
+};
