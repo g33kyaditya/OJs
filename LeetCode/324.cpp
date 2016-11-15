de739f46f69fda08768d2a4dbbafe30001f35740
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-11-15 21:54:29 +0530

    Wiggle Sort II

---
+++ LeetCode/324.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int mid = nums.size()/2;
+        if (nums.size()%2 == 0 && mid >= 1)
+            mid--;
+        
+        int last = nums.size() - 1;
+        vector<int> v;
+        int m = mid;
+        while (mid >= 0) {
+            v.push_back(nums[mid]);
+            mid--;
+            if (last != m) {
+                v.push_back(nums[last]);
+                last--;
+            }
+        }
+        nums.clear();
+        nums = v;
+    }
+};
