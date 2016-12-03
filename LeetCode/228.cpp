87e52d8390b58b95dfab92938733c556f3af5bcd
Aditya Dev Sharma <aditya.sharma15696@gmail.com>
2016-12-03 12:11:54 +0530

    Summary Ranges

---
+++ LeetCode/228.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string createRange(int lo, int hi) {
+        if (lo == hi)
+            return std::to_string(lo);
+        return std::to_string(lo) + "->" + std::to_string(hi);
+    }
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        if (nums.empty())
+            return ans;
+        
+        int lo = nums[0];
+        int i;
+        for (i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i-1] + 1)
+                continue;
+            else {
+                string range = createRange(lo, nums[i-1]);
+                lo = nums[i];
+                ans.push_back(range);
+            }
+        }
+        
+        string range = createRange(lo, nums[i-1]);
+        lo = nums[i];
+        ans.push_back(range);
+        return ans;
+    }
+};
